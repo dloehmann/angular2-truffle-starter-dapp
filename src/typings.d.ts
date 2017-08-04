1f9aaf7b84dcfcd6577f11747deeccac099c4d98
Dennis Löhmann <U553363@WI7-LHIND2581.ads.dlh.de>
2017-08-04 20:22:21 +0200

    settings

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,6 +1,20 @@
 /* SystemJS module definition */
-declare var module: {
-  id: string;
-};
+// declare var module: {
+//   id: string;
+// };
 
-declare var require: NodeRequire;
+// declare var require: NodeRequire;
+
+
+
+
+/* SystemJS module definition */
+declare var module: NodeModule;
+interface NodeModule {
+  id: string;
+}
+
+declare module "*.json" {
+  const value: any;
+  export default value;
+}
